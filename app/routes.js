53225b52d93884583c174bbbb3c230bbc124b2a3
Naila Nur <nnur3@uwo.ca>
2015-06-25 15:06:25 -0400

    working on sending img

---
+++ app/routes.js
@@ -0,0 +1,52 @@
+//================================Routes===========================================//
+
+module.exports = function(app) {
+
+    //=============================== DUMMY ===========================================
+
+
+    //send website all blog text
+    app.get('/blogtext', function(req, res) {
+        res.send(storage.values());
+
+
+    });
+
+
+    //send website all images
+
+    app.get('/blogimg', function(req, res) {
+        res.type('jpg');
+        res.sendFile(path.join(__dirname, '../public/images', 'book.jpg'));
+    });
+
+
+    //get image data from client and save in img dir
+    app.post('/img', multipartyMiddleware, function(req, res) {
+        var file = req.files.file;
+        console.log(file.path);
+
+
+        fs.readFile(file.path, function(err, data) {
+            // ...
+
+            fs.writeFile('public/images/' + file.name, data, function(err) {
+
+            });
+        });
+
+    });
+
+
+    //get blog data from client
+    app.post('/blog', function(req, res) {
+
+        storage.setItem(req.body.title, req.body);
+
+        res.send('thank you come again')
+    });
+
+
+
+
+}
